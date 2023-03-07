15283d8411887b10b3ac92135abeba8fc21e9073
pernillamoreno <113672077+pernillamoreno@users.noreply.github.com>
2023-03-07 19:22:17 +0100

    Update TrafficDetails.java

--- PublicTransport/src/main/java/com/example/publictransport/model/TrafficDetails.java
+++ PublicTransport/src/main/java/com/example/publictransport/model/TrafficDetails.java
@@ -8,20 +8,6 @@ import java.util.List;
 @Data
 public class TrafficDetails {
 
-    @JsonAlias("Trip")
-    private TripDetails[] trip;
-
-    @Data
-    public static class TripDetails {
-
-        @JsonAlias("ServiceDays")
-        private ServiceDaysDetails[] serviceDays;
 
 
-        @Data
-        public static class ServiceDaysDetails {
-            private String planningPeriodBegin;
-            private String planningPeriodEnd;
-        }
-    }
-}*/
+}
